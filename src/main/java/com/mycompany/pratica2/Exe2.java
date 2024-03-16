d258df5acd57931c6e69cffce83eaa2ce28a9ecb
Fernanda Nunes Oliveira Santos <fernandanos@hotmail.com>
2024-03-15 22:20:05 -0300

    Terminando o exercício

--- src/main/java/com/mycompany/pratica2/Exe2.java
+++ src/main/java/com/mycompany/pratica2/Exe2.java
@@ -9,8 +9,16 @@ package com.mycompany.pratica2;
  * @author Fernanda
  */
 public class Exe2 {
-    public static String Oi(){
-        return "oi";
+    public static int Soma(int a, int b){
+       return a+b;
     }
-    
+    public static int Sub(int num1, int num2){
+        return num1-num2;
+    }
+    public static int Mult(int mult1,int mult2){
+        return mult1*mult2;
+    }
+    public static int Potencia(int potencia1, int potencia2){
+        return (int) Math.pow(potencia1, potencia2);
+    }
 }
--- src/main/java/com/mycompany/pratica2/Pratica2.java
+++ src/main/java/com/mycompany/pratica2/Pratica2.java
@@ -8,8 +8,30 @@ package com.mycompany.pratica2;
  *
  * @author Fernanda
  */
+import java.util.Scanner;
 public class Pratica2 {
+    static Scanner input=new Scanner(System.in);
     public static void main(String args[]){
-        System.out.println(Exe2.Oi());
+       
+        System.out.println("soma(int a , int b): retorna a soma de a com b");
+        int a=input.nextInt(); 
+        int b=input.nextInt();
+        System.out.println(Exe2.Soma(a, b));
+        
+        System.out.println("subtracao(int a, int b): retorna a diferença entre a e b");
+        int num1=input.nextInt(); 
+        int num2=input.nextInt();
+        System.out.println(Exe2.Sub(num1, num2));
+        
+        System.out.println("multiplicacao(int a, int b): retorna a multiplicação de a por b");
+        int mult1=input.nextInt(); 
+        int mult2=input.nextInt();
+        System.out.println(Exe2.Mult(mult1, mult2));
+        
+        System.out.println("potencia(int a, int b): retorna o valor de a elevado a b");
+        int potencia1=input.nextInt(); 
+        int potencia2=input.nextInt();
+        System.out.println(Exe2.Potencia(potencia1, potencia2));
+        
     }
 }
